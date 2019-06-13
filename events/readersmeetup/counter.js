14312d37cf462633bd086af16ce1e50ddc849f30
RaxRaj <raj.fungus@gmail.com>
2019-06-13 15:09:20 +0530

    ALPHA VERSION

--- events/readersmeetup/counter.js
+++ events/readersmeetup/counter.js
@@ -1,6 +1,6 @@
 
 $(".countdown")
-    .countdown("2019/09/22", function(event) {
+    .countdown("2019/06/22", function(event) {
         $(this).html(
             event.strftime('<div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
         );
