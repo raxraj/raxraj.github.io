d71db65b9c571bad40dd9706f26d1989b2735e9d
indianprogaming <ipgbycurlybrackets@gmail.com>
2019-09-10 18:14:23 +0530

    PORTFOLIO ADDED

--- events/readersmeetup/counter.js
+++
@@ -1,7 +0,0 @@
-
-$(".countdown")
-    .countdown("2019/06/22", function(event) {
-        $(this).html(
-            event.strftime('<div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
-        );
-    });
